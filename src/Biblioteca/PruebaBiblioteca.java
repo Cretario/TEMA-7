ae5e35438a2a38e440c29a6bdf717a69f30cc695
Andres Masset <masset.ruiz151@gmail.com>
2019-03-12 12:27:03 +0100

    Actualizacion

--- src/Biblioteca/PruebaBiblioteca.java
+++ src/Biblioteca/PruebaBiblioteca.java
@@ -10,23 +10,42 @@ public class PruebaBiblioteca {
 	static Scanner sc = new Scanner(System.in);
 	public static void main (String[]args) {
 
-	
 		
-		Libro[] libro = new Libro[3];
+	ArrayList<String> libros = new ArrayList<String>();
+	Libro[] libro = new Libro[libros.size()];	
+	libros.add("GGRR MARTIN "+"JUEGO DE TRONOS");
+		libros.add("GGRR MARTIN "+"CHOQUE DE REYES");
+		libros.add("GGRR MARTIN "+"TORMENTAS DE ESPADAS");
+		libros.add("GGRR MARTIN "+"FESTIN DE CUERVOS");
+		libros.add("GGRR MARTIN "+"DANZA DE DRAGONES");
+		libros.add("GGRR MARTIN "+"VIENTOS DE INVIERNO");
+		libros.add("GGRR MARTIN "+"SUEÑOS DE PRIMAVERA");
+		libros.add("GGRR MARTIN "+"FUEGO Y SANGRE");
+				
+			
 
-		libro[0] = new Libro ("Juego de Tronos","GIGAMESHT","GGRRMARTIN", " ");
-		libro[1] = new Libro ("Choque de Reyes","GIGAMESHT","GGRRMARTIN", " ");
-		libro[2] = new Libro ("Tormenta de Espadas","GIGAMESHT","GGRRMARTIN", " ");
+		ArrayList<String> usuarios  = new ArrayList<String>();
+		Usuario[] usuario  = new Usuario[usuarios.size()];
+		usuarios.add(" "+"Cretario "+"5/07/1998");
+		usuarios.add(" "+"Pedro "+"5/05/2019");
+		usuarios.add(" "+"Juan "+"23/04/2000");
 
-		Usuario[] usuarios  = new Usuario[3];
-
-		usuarios[0] = new Usuario(" ","Cretario", "5/07/1998");
-		usuarios[1] = new Usuario(" ","Pedro", "5/05/2019");
-		usuarios[2] = new Usuario(" ","Pedro", "23/04/2000");
-
+		
+		
+		
+		for(int i=0;i<libros.size();i++) {
+			System.out.println(libros.get(i));
+		}
+		
+		for(int i=0;i<usuarios.size();i++) {
+			System.out.println(usuarios.get(i));
+		}
+		
 		//usuarios[1].AlquilarPublicacion(libro[1]);
 		//usuarios[2].AlquilarPublicacion(libro[2]);
-		System.out.println("Elige una opcion \n 1-AlquilarLibro \n 2-DevolverLibro");
+		
+	
+		System.out.println("Elige una opcion \n 1-AlquilarLibro \n 2-DevolverLibro \n 3-HistorialdeUsuario");
 		int opcion;
 		
 		do {
@@ -35,6 +54,7 @@ public class PruebaBiblioteca {
 			int coduser = 0;
 			int codlibro = 0;
 			
+		
 			switch(opcion) {
 
 			case 1:
@@ -42,8 +62,10 @@ public class PruebaBiblioteca {
 				System.out.println("Introduce tu codigo de usuario");
 				coduser=sc.nextInt();
 				System.out.println("Introduce el codigo del libro que quieras alquilas");
+			
 				codlibro=sc.nextInt();
-				usuarios[coduser].AlquilarPublicacion(libro[codlibro]);	
+				usuario[usuarios.get(0)].AlquilarPublicacion(libros.get(codlibro));
+				
 
 
 
@@ -57,8 +79,14 @@ public class PruebaBiblioteca {
 				coduser=sc.nextInt();
 				System.out.println("Introduce el Codigo del Libro");
 				codlibro=sc.nextInt();
-				usuarios[coduser].DevolverPublicacion(libro[codlibro]);
+				usuarios[coduser].DevolverPublicacion(null,coduser,libros.get(codlibro));
 				break;
+				
+			case 3:
+				
+				System.out.println("Introduce el numero de Historial de Usuario");
+				coduser=sc.nextInt();
+				usuarios[coduser].HistorialFecha(coduser);
 			}
 
 	
@@ -73,8 +101,12 @@ public class PruebaBiblioteca {
 
 
 		//ArrayList<Date> date = new ArrayList<Date>() ;
-		//date.add(u1.AlquilarPublicacion(libro));
+	
+	
+	//date.add(u1.AlquilarPublicacion(libro));
 
 	}
 
+
 }
+			
--- src/Biblioteca/Usuario.java
+++ src/Biblioteca/Usuario.java
@@ -11,10 +11,15 @@ protected String nombre;
 Calendar c1 = Calendar.getInstance();
 Calendar c2 = new GregorianCalendar();
 
-String dia = Integer.toString(c2.get(Calendar.DATE));
+String dia = Integer.toString(c1.get(Calendar.DATE));
 String mes = Integer.toString(c2.get(Calendar.MONTH));
-String año = Integer.toString(c2.get(Calendar.YEAR));
-ArrayList<String>fechaAl=new ArrayList<String>();
+String año = Integer.toString(c1.get(Calendar.YEAR));
+
+String Fecha =dia.concat("/"+mes+"/"+año);
+
+Integer diaL =c2.get(Calendar.DATE+7);
+ArrayList<String> fechaH = new ArrayList<String>();
+
 public Usuario(String dni,String nombre,String fecha) {
 this.dni=dni;
 this.nombre=nombre;
@@ -38,16 +43,16 @@ public void setNombre(String nombre) {
 this.nombre=nombre;	
 }
 
-public void setFecha(ArrayList fechaA1) {
-	this.fechaAl=fechaA1;
+public void setFecha(ArrayList fechaH) {
+	this.fechaH=fechaH;
 }
 
-protected void AlquilarPublicacion(Publicacion publicacion) {
+protected void AlquilarPublicacion(Publicacion publicacion,ArrayList libros) {
 if(publicacion.isDisponible()&& publicacion.getDniUsuario()== this.dni) {
 publicacion.setDisponible(false);
 publicacion.setDniUsuario(dni);
-this.setFecha(fechaAl);
-System.out.println(" el Articulo "+publicacion.getNombre()+" ha sido alquilado"+" por "+this.getNombre());
+fechaH.add(Fecha+""+this.getNombre());
+System.out.println(" el Articulo "+publicacion.getNombre()+" ha sido alquilado"+" por "+this.getNombre()+" en la fecha "+Fecha);
 }else {
 System.out.println(publicacion.getNombre()+" no esta disponible para el alquiler");	
 }
@@ -55,20 +60,39 @@ System.out.println(publicacion.getNombre()+" no esta disponible para el alquiler
 
 
 }
-protected void DevolverPublicacion(Publicacion publicacion) {
+protected void DevolverPublicacion(Publicacion publicacion,int coduser) {
 if(!publicacion.isDisponible()&& publicacion.getDniUsuario()== this.dni) {
 publicacion.setDisponible(true);
 publicacion.setDniUsuario(dni);
 System.out.println(" El Articulo "+publicacion.getNombre()+" ha sido devuelto"+" por "+this.getNombre());
+
+int numHistorial=fechaH.size();
+
+
+
 }else {
 System.out.println(publicacion.getNombre()+" no puedes devolver este articulo debido a que no lo has alquilado");	
 }
 
-if(c2.after(fechaAl)) {
-System.out.println("Devolucion fuera de tiempo");	
-}
+for(int i=0;i<fechaH.size();i++)
+
+if(this.Fecha==fechaH.get(coduser)+7) {
+
+System.out.println("Devolucion fuera de tiempo");
 
 
 }
-
+	
 }
+	
+
+
+
+protected void HistorialFecha(int coduser) {
+	
+	System.out.println(this.fechaH.get(0));
+	
+}
+}
+
+
